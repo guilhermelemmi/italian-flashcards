97517a60572c5813c2679147aa17eab389bde248
Guilherme Lemmi <guilhermelemmi@gmail.com>
2019-07-24 22:12:54 -0300

    improve layout

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,9 +4,6 @@ import { Button } from 'antd';
 const Intro = (props) => {
   return (
     <div className="Intro">
-      <p>
-        This is a Flashcard Game for people studying Javascript
-      </p>
       <Button type="primary" onClick={props.onPlay}>Play</Button>
     </div>
   );
